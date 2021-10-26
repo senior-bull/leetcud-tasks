b9dc3cefdf838f1a349b18fe8debd3ec463151d1
Vitaliy Bull <14792518+senior-bull@users.noreply.github.com>
2021-10-26 21:55:26 +0400

    Add solution

--- src/main/java/com/vito/eeasy/LargestValueInTreeRow.java
+++ src/main/java/com/vito/eeasy/LargestValueInTreeRow.java
@@ -1,2 +1,30 @@
-package com.vito.eeasy;public class LargestValueInTreeRow {
+package com.vito.eeasy;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class LargestValueInTreeRow {
+
+    public List<Integer> largestValues(TreeNode root) {
+        if (root == null) {
+            return Collections.emptyList();
+        }
+
+        List<TreeNode> currentLevel = Collections.singletonList(root);
+        List<Integer> result = new ArrayList<>();
+
+        while (!currentLevel.isEmpty()) {
+
+            int maxValue = currentLevel.stream().mapToInt(node -> node.val).max().orElse(0);
+            result.add(maxValue);
+
+            currentLevel = currentLevel.stream()
+                    .flatMap(node -> Stream.of(node.left, node.right))
+                    .filter(Objects::nonNull)
+                    .collect(Collectors.toList());
+        }
+
+        return result;
+    }
 }
