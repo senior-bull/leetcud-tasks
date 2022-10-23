c1502f75ebb6a1ce6bca27af264a2b6605321a16
Vitaliy Bykov <14792518+senior-bull@users.noreply.github.com>
2022-10-23 11:53:05 +0300

    Styling

--- src/main/java/com/vito/med/DailyTemperatures.java
+++ src/main/java/com/vito/med/DailyTemperatures.java
@@ -4,7 +4,7 @@ import java.util.Stack;
 
 public class DailyTemperatures {
 
-    /*
+    /**
     * Given an array of integers temperatures represents the daily temperatures,
     * return an array answer such that answer[i] is the number of days you have to
     * wait after the ith day to get a warmer temperature. If there is no future
