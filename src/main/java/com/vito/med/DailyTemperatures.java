1367277c86458a15ae0bf213fd10261b0252afcc
Vitaliy Bykov <14792518+senior-bull@users.noreply.github.com>
2022-10-23 11:45:05 +0300

    Styling

--- src/main/java/com/vito/med/DailyTemperatures.java
+++ src/main/java/com/vito/med/DailyTemperatures.java
@@ -5,7 +5,10 @@ import java.util.Stack;
 public class DailyTemperatures {
 
     /*
-    * Given an array of integers temperatures represents the daily temperatures, return an array answer such that answer[i] is the number of days you have to wait after the ith day to get a warmer temperature. If there is no future day for which this is possible, keep answer[i] == 0 instead.
+    * Given an array of integers temperatures represents the daily temperatures,
+    * return an array answer such that answer[i] is the number of days you have to
+    * wait after the ith day to get a warmer temperature. If there is no future
+    * day for which this is possible, keep answer[i] == 0 instead.
     */
     public int[] dailyTemperatures(int[] temperatures) {
         Stack<Integer> stack = new Stack<>();
